fb5b146ba0378832d1baeddede0192af565b262f
Jose Reyes Palacio <jrpalacio.dev@gmail.com>
2025-11-06 21:35:39 -0600

    feat(teams): Agregar datos iniciales de los equipos con logotipos y nombres

---
+++ app/data/teams.ts
@@ -0,0 +1,59 @@
+ const teams = [
+  {
+    id: 1,
+    name: 'F.C. Barcelona',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/paYnEE8hcrP96neHRNofhQ_64x64.png'
+  },
+  {
+    id: 2,
+    name: 'Real Madrid',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/Th4fAVAZeCJWRcKoLW7koA_96x96.png'
+  },
+  {
+    id: 3,
+    name: 'Villarreal',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/WKH7Ak5cYD6Qm1EEqXzmVw_96x96.png'
+  },
+  {
+    id: 4,
+    name: 'Athletic Club',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/OSL_5Zm6kYPP1J17Bo5uDA_96x96.png'
+  },
+  {
+    id: 5,
+    name: 'Alavés',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/RUcTOeFcBc7q7uku1nR_iQ_96x96.png'
+  },
+  {
+    id: 6,
+    name: 'PSV Eindhoven',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/FylTPR72JXIGFzqwQocSAQ_96x96.png'
+  },
+  {
+    id: 2,
+    name: 'Real Madrid',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/Th4fAVAZeCJWRcKoLW7koA_96x96.png'
+  },
+  {
+    id: 3,
+    name: 'Villarreal',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/WKH7Ak5cYD6Qm1EEqXzmVw_96x96.png'
+  },
+  {
+    id: 4,
+    name: 'Athletic Club',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/OSL_5Zm6kYPP1J17Bo5uDA_96x96.png'
+  },
+  {
+    id: 5,
+    name: 'Alavés',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/RUcTOeFcBc7q7uku1nR_iQ_96x96.png'
+  },
+  {
+    id: 6,
+    name: 'PSV Eindhoven',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/FylTPR72JXIGFzqwQocSAQ_96x96.png'
+  },
+]
+
+export default teams
