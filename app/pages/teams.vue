962ed6f7e384e38b3caa68165a713b9844003ada
Jose Reyes Palacio <jrpalacio.dev@gmail.com>
2025-11-06 21:35:24 -0600

    feat(teams): Añade una página de equipos con listado y estilo dinámicos.

---
+++ app/pages/teams.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+    import teams from'@/data/teams'
+</script>
+<template>
+<div>
+    <section>
+    <header>
+        <h2 class="text-3xl">Equipos</h2>
+        <p>Torneo <strong>Enrique Alfaro</strong> 2025</p>
+    </header>
+</section> 
+<section>
+     <ul class="galery">
+        <li v-for="team in teams" :key="team.id" class="p-2 border border-amber-500 grid justify-center rounded">
+            <img :src="team.logo" :alt="team.name">
+            {{ team.name }}
+        </li>
+     </ul>
+</section>
+</div>
+</template>
+
+<style scoped>
+.galery {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+    gap: 1rem;
+}
+</style>
