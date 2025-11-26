5788cf2c54e01b7d93486016900b0df6b9a3889d
Jose Reyes Palacio <jrpalacio.dev@gmail.com>
2025-11-26 08:49:10 -0600

    feat(footer): Añadir componente de pie de página con enlaces y botones de redes sociales

---
+++ app/components/footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <footer class="flex flex-col-reverse items-center gap-4 lg:flex-row lg:justify-between py-8 px-4 max-w-6xl mx-auto">
+    <section>
+      <p class="text-muted text-sm">
+          Copyright © {{ new Date().getFullYear() }}
+      </p>
+    </section>
+    <section>
+      <ul class="flex flex-row gap-4">
+        <li>
+          <a href="https://jrpalacio-dev.vercel.app/" rel="noopener" target="_blank" class="text-muted text-sm">
+            <span class="truncate">
+              jrpalacio.dev
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="https://elc1elo.com" rel="noopener" target="_blank" class="text-muted text-sm ">
+            <span class="truncate">
+              elc1elo.com
+            </span>
+          </a>
+        </li>
+      </ul>
+    </section>
+    <section>
+      <UButton
+          icon="i-simple-icons-instagram"
+          color="neutral"
+          variant="ghost"
+          to="https://www.instagram.com/el.cielosoccer"
+          target="_blank"
+          aria-label="Instagram"
+        />
+        <UButton
+          icon="i-simple-icons-facebook"
+          color="neutral"
+          variant="ghost"
+          to="https://www.facebook.com/elcielosoccer"
+          target="_blank"
+          aria-label="Facebook"
+        />
+    </section>
+  </footer>
+  </template>
