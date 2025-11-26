f492d2c2f3abdda4c56266ec91be7188baf5bd08
Jose Reyes Palacio <jrpalacio.dev@gmail.com>
2025-11-26 08:52:11 -0600

    feat(teams): Refactorizar el diseño de la página de equipos y actualizar el componente de visualización del equipo

--- app/pages/teams.vue
+++ app/pages/teams.vue
@@ -1,29 +1,27 @@
 <script setup lang="ts">
     import teams from'@/data/teams'
+
+    // custom layout 
+    definePageMeta({
+        layout: 'public-layout'
+    })
 </script>
 <template>
-<div>
-    <section>
-    <header>
+    <header class="pt-4 pb-8">
         <h2 class="text-3xl">Equipos</h2>
         <p>Torneo <strong>Enrique Alfaro</strong> 2025</p>
     </header>
-</section> 
-<section>
-     <ul class="galery">
-        <li v-for="team in teams" :key="team.id" class="p-2 border border-amber-500 grid justify-center rounded">
-            <img :src="team.logo" :alt="team.name">
-            {{ team.name }}
-        </li>
-     </ul>
-</section>
-</div>
+    <section>
+        <ul class="galery">
+            <team-card v-for="team in teams" :key="team.id" :team="team" />
+        </ul>
+    </section>
 </template>
 
 <style scoped>
 .galery {
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
     gap: 1rem;
 }
 </style>
