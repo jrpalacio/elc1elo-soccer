e95874d51a6be705440f356b04c9843a0b156647
Jose Reyes Palacio <jrpalacio.dev@gmail.com>
2025-11-26 08:50:23 -0600

    feat(teams): Actualizar los nombres de los equipos y agregar nuevos equipos al conjunto de datos

--- app/data/teams.ts
+++ app/data/teams.ts
@@ -1,3 +1,5 @@
+import { id } from "@nuxt/ui/runtime/locale/index.js"
+
  const teams = [
   {
     id: 1,
@@ -6,7 +8,7 @@
   },
   {
     id: 2,
-    name: 'Real Madrid',
+    name: 'F. C. Deportivo Real Madrid, Español S.A.D.',
     logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/Th4fAVAZeCJWRcKoLW7koA_96x96.png'
   },
   {
@@ -30,30 +32,75 @@
     logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/FylTPR72JXIGFzqwQocSAQ_96x96.png'
   },
   {
-    id: 2,
-    name: 'Real Madrid',
+    id: 7,
+    name: 'Girona FC',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/sHiSmLm_rA0BOC5MfrNt8A_96x96.png'
+  },
+  {
+    id: 8,
+    name: 'Celta de Vigo',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/wpdhixHP_sloegfP0UlwAw_96x96.png'
+  },
+  {
+    id: 9,
+    name: 'Real Madrid Club de Fútbol',
     logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/Th4fAVAZeCJWRcKoLW7koA_96x96.png'
   },
   {
-    id: 3,
-    name: 'Villarreal',
-    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/WKH7Ak5cYD6Qm1EEqXzmVw_96x96.png'
+    id: 10,
+    name: 'Rayo Vallecano',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/i5LifmxEVIl0sbvIysiyhw_96x96.png'
   },
   {
-    id: 4,
-    name: 'Athletic Club',
-    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/OSL_5Zm6kYPP1J17Bo5uDA_96x96.png'
+    id: 11,
+    name: 'R.C.D. Mallorca',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/Ss21P4CUmigjrEtcoapjVg_96x96.png'
   },
   {
-    id: 5,
+    id: 12,
+    name: 'Getafe C.F.',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/1UDHZMdKZD15W5gus7dJyg_96x96.png'
+  },
+  {
+    id: 13,
     name: 'Alavés',
     logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/RUcTOeFcBc7q7uku1nR_iQ_96x96.png'
   },
   {
-    id: 6,
-    name: 'PSV Eindhoven',
-    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/FylTPR72JXIGFzqwQocSAQ_96x96.png'
+    id: 14,
+    name: 'Real Oviedo',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/2hbZVQulcYqiPFXkWDkB_g_96x96.png'
   },
+  {
+    id: 15,
+    name: 'Ourense CF',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/zjzVbPDn_ew_5x8Qh29j2A_96x96.png'
+  },
+  {
+    id: 16,
+    name: 'Osasuna',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/pk-hNCNpGM_JDoHHvLVF-Q_96x96.png'
+  },
+  {
+    id: 17,
+    name: 'Levante UD',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/SQB-jlVosxVV1Ce79FhbOA_96x96.png'
+  },
+  {
+    id: 18,
+    name: 'RCD Espanyol',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/TKitIqelDyN6M-kYt4Uc0g_96x96.png'
+  },
+  {
+    id: 19,
+    name: 'Real Betis',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/XDClkrMKtkm3UTP2YKN6oQ_96x96.png'
+  },
+  {
+    id: 20,
+    name: 'Lyon',
+    logo: 'https://ssl.gstatic.com/onebox/media/sports/logos/SrKK55dUkCxe4mJsyshfCg_96x96.png'
+  }
 ]
 
 export default teams
